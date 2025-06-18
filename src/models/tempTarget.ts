26b3c0166deac4c134d33d9f8119f1de69f872af
jbeenenga <j.beenenga@gmail.com>
2025-06-18 10:18:21 +0200

    Add JSDoc comments to TempTarget interface
    - Add interface-level JSDoc comment
    - Add JSDoc comments for all properties
    - Clarify that 'until' can be either a time or special keywords

--- src/models/tempTarget.ts
+++ src/models/tempTarget.ts
@@ -1,4 +1,9 @@
+/**
+ * Interface representing a target temperature configuration
+ */
 export interface TempTarget {
+	/** Target temperature in degrees */
 	temp: number;
+	/** Time until which this temperature is valid (HH:MM format or "boost"/"pause") */
 	until: string;
 }
